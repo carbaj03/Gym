6580e0765df695e50609a3e1e95fd951ed46d74a
alejandro <alejandro.carbajo@mo2o.com>
2017-07-12 13:27:38 +0200

    Replace 'Command' to 'Request' in ExerciseType service

--- domain/src/main/java/com/acv/gym/domain/service/exercise/type/ViewExerciseType.kt
+++ domain/src/main/java/com/acv/gym/domain/service/exercise/type/ViewExerciseType.kt
@@ -5,15 +5,21 @@ import com.acv.gym.domain.GymGateway
 import com.acv.gym.domain.model.ExerciseType
 import com.acv.gym.domain.service.ExerciseTypeCommand
 import com.acv.gym.domain.service.GymUseCase
+import com.acv.gym.domain.service.Id
+import com.acv.gym.domain.service.Request
 import kategory.Either
 import kategory.Option
 
 
 open class ViewExerciseType(
         val gateway: GymGateway
-) : GymUseCase<ExerciseTypeCommand, List<ExerciseType>> {
-    override fun execute(input: Option<ExerciseTypeCommand>) = when (input) {
+) : GymUseCase<ViewExerciseTypeRequest, List<ExerciseType>> {
+    override fun execute(input: Option<ViewExerciseTypeRequest>) = when (input) {
         is Option.None -> Either.Left(GenericError.NetworkError)
         is Option.Some -> gateway.getExerciseTypesBy(input.value.id)
     }
 }
+
+data class ViewExerciseTypeRequest(
+        val id: Id
+) : Request
