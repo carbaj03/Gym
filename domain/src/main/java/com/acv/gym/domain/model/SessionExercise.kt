28024ad044fbcb1e05777dcbb8210dc482d2e7af
alejandro <alejandro.carbajo@mo2o.com>
2017-07-12 09:50:18 +0200

    feat: Add "SessionExerciseId"

--- domain/src/main/java/com/acv/gym/domain/model/SessionExercise.kt
+++ domain/src/main/java/com/acv/gym/domain/model/SessionExercise.kt
@@ -1,10 +1,15 @@
 package com.acv.gym.domain.model
 
 import com.acv.gym.domain.usecase.Id
+import java.util.*
 
 data class SessionExercise(
-        val id: Id = Id(),
+        val id: SessionExerciseId = SessionExerciseId(),
         val exercise: Exercise = Exercise("","","",""),
         val sets: List<SessionSet> = listOf(),
         val session: Id = Id("")
 ) : Model
+
+data class SessionExerciseId(
+        val value: String = UUID.randomUUID().toString()
+)
