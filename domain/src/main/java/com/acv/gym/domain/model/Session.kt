d1894de7c530ed6da0c111ce4d9ff9be24731106
alejandro <alejandro.carbajo@mo2o.com>
2017-07-12 09:02:33 +0200

    feat: Add "SessionId"

--- domain/src/main/java/com/acv/gym/domain/model/Session.kt
+++ domain/src/main/java/com/acv/gym/domain/model/Session.kt
@@ -1,11 +1,13 @@
 package com.acv.gym.domain.model
 
-import com.acv.gym.domain.usecase.Id
 import java.util.*
 
-
 data class Session(
-        val id: Id,
+        val id: SessionId,
         val name: String,
         val date: Date
 ) : Model
+
+data class SessionId(
+        val value: String = UUID.randomUUID().toString()
+)
